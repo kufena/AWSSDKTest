fe492fff284b8c431781ede9a52a7ed6f6cb9804
Andrew Douglas <potterboy67@outlook.com>
2020-08-25 17:23:34 +0100

    Add project files.

---
+++ AWSSDKTest/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Threading.Tasks;
+using Amazon;
+using Amazon.S3;
+using Amazon.S3.Model;
+using Amazon.S3.Transfer;
+
+namespace AWSSDKTest
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var client = new AmazonS3Client(RegionEndpoint.EUWest2);
+            ListBucketsResponse buckets_response = await client.ListBucketsAsync();
+            
+            foreach(var buckets in buckets_response.Buckets)
+            {
+                Console.WriteLine(buckets.BucketName + " date:" + buckets.CreationDate);
+            }
+
+        }
+    }
+}

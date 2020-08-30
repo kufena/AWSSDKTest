1f15821c2fb779276bc9f0950d1a322a831f5c1b
Andrew Douglas <potterboy67@outlook.com>
2020-08-30 18:55:10 +0100

    Checks contents now.  Is there a way of telling a folder from a file?

--- AWSSDKTest/Program.cs
+++ AWSSDKTest/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.ComponentModel.DataAnnotations;
+using System.Linq;
 using System.Threading.Tasks;
 using Amazon;
 using Amazon.S3;
@@ -19,9 +20,18 @@ namespace AWSSDKTest
             {
                 ListObjectsV2Request req = new ListObjectsV2Request();
                 req.BucketName = bucket.BucketName;
+                // NOTE if this finds a folder it includes the folder as an object, and its content as
+                // more objects.  I thought folders didn't exist as such, but it seems they do.
                 ListObjectsV2Response res = await client.ListObjectsV2Async(req);
                 int len = res.S3Objects.Count;
                 Console.WriteLine(bucket.BucketName + " date:" + bucket.CreationDate + " has " + len + " objects.");
+                if (len > 0)
+                {
+                    foreach(var obj in res.S3Objects)
+                    {
+                        Console.WriteLine("   " + obj.Key + " " + obj.Size + " " + obj.StorageClass + " " + obj.ETag);
+                    }
+                }
             }
 
             Console.WriteLine("Thanks for all the fish");

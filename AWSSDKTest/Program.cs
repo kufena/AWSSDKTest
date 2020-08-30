92565b0b01232b7c2d978ed6538794551f3099b1
Andrew Douglas <potterboy67@outlook.com>
2020-08-30 18:50:07 +0100

    Added a count top level objects - atleast i think that's what it does.

--- AWSSDKTest/Program.cs
+++ AWSSDKTest/Program.cs
@@ -15,9 +15,13 @@ namespace AWSSDKTest
             var client = new AmazonS3Client(RegionEndpoint.EUWest2);
             ListBucketsResponse buckets_response = await client.ListBucketsAsync();
             
-            foreach(var buckets in buckets_response.Buckets)
+            foreach(var bucket in buckets_response.Buckets)
             {
-                Console.WriteLine(buckets.BucketName + " date:" + buckets.CreationDate);
+                ListObjectsV2Request req = new ListObjectsV2Request();
+                req.BucketName = bucket.BucketName;
+                ListObjectsV2Response res = await client.ListObjectsV2Async(req);
+                int len = res.S3Objects.Count;
+                Console.WriteLine(bucket.BucketName + " date:" + bucket.CreationDate + " has " + len + " objects.");
             }
 
             Console.WriteLine("Thanks for all the fish");
